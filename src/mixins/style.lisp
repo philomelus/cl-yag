7eeb822923d3cc93d1b2bbe7e19b564100e27308
Russ Gibson <russg@rnstech.com>
2025-08-25 13:52:21 -0700

    Added style-3d-mixin.

---
+++ src/mixins/style.lisp
@@ -0,0 +1,14 @@
+(in-package #:cl-yag)
+
+;;;; style-3d-mixin ===========================================================
+
+(defvar +STYLE-3D-MIXIN-OPTIONS+ '(:default :inset :outset :flat))
+
+(defclass style-3d-mixin ()
+  ((style :initarg :style :type keyword :initform :default :accessor style)))
+
+(defmethod initializa-instance :after ((object style-3d-mixin) &key)
+  (with-slots (style) object
+    (unless (member style +STYLE-3D-MIXIN-OPTIONS+)
+      (error "unknown style 3d option, got: ~a" style)))
+  (my-next-method))
