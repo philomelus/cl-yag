ea18057ca26b9457270ec80fc6442f01af3c51a0
Russ Gibson <russg@rnstech.com>
2025-09-22 13:18:17 -0700

    Added title-color-mixin.

--- src/mixins/color.lisp
+++ src/mixins/color.lisp
@@ -2,12 +2,12 @@
 
 (declaim (optimize (debug 3) (speed 0) (safety 3)))
 
-;;;; color-mixin ==============================================================
+;;;; COLOR-MIXIN ==============================================================
 
 (defclass color-mixin ()
   ((color :initarg :color :initform nil :type list :accessor color)))
 
-;;;; color-3d-mixin ===========================================================
+;;;; COLOR-3D-MIXIN ===========================================================
 
 (defclass color-3d-mixin ()
   ((normal :initarg :normal :initform nil :accessor normal-color)
@@ -16,29 +16,33 @@
    (very-dark :initarg :very-dark :initform nil :accessor very-dark-color)
    (very-light :initarg :very-light :initform nil :accessor very-light-color)))
 
-;;;; back-color-mixin =========================================================
+;;;; BACK-COLOR-MIXIN =========================================================
 
 (defclass back-color-mixin ()
   ((back-color :initarg :back-color :initform nil :accessor back-color)))
 
-;;;; fore-color-mixin =========================================================
+;;;; FORE-COLOR-MIXIN =========================================================
 
 (defclass fore-color-mixin ()
   ((fore-color :initarg :fore-color :initform nil :accessor fore-color)))
 
-;;;; back-fore-color-mixin ====================================================
+;;;; BACK-FORE-COLOR-MIXIN ====================================================
 
 (defclass back-fore-color-mixin (back-color-mixin
                                  fore-color-mixin)
   ())
 
-;;;; frame-color-mixin ========================================================
+;;;; FRAME-COLOR-MIXIN ========================================================
 
 (defclass frame-color-mixin ()
   ((frame-color :initarg :frame-color :initform nil :accessor frame-color)))
 
-;;;; interior-color-mixin =====================================================
+;;;; INTERIOR-COLOR-MIXIN =====================================================
 
 (defclass interior-color-mixin ()
   ((interior-color :initarg :interior-color :initform nil :accessor interior-color)))
 
+;;;; TITLE-COLOR-MIXIN ========================================================
+
+(defclass title-color-mixin ()
+  ((title-color :initarg :title-color :initform nil :accessor title-color)))
