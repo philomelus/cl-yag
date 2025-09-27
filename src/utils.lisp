b915e68799f7b60bbf38df62e26dd8b3b3bd31d5
Russ Gibson <russg@rnstech.com>
2025-09-27 03:44:05 -0700

    Removed must-init (leftover from original import).

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,22 +1,6 @@
 (in-package :cl-yag)
 
-(declaim (optimize (debug 3) (speed 0) (safety 3)))
-
-;; (defmacro must-init (test desc)
-;;   (typecase test
-;;     (foreign-pointer
-;;      `(when (null-pointer-p ,test)
-;;         (if (> (al:get-errno) 0)
-;;             (error "Failed to initialize ~s (~d)" ,desc (al:get-errno))
-;;             (error "Failed to initialize ~s" ,desc))))
-;;     (boolean
-;;      `(unless ,test
-;;         (if (> (al:get-errno) 0)
-;;             (error "Failed to initialize ~s (~d)" ,desc (al:get-errno))
-;;             (error "Failed to initialize ~s" ,desc))))
-;;     (cons) <<<--- Don't know how to deal with this ... (turns out ... its a list ...)
-;;     (otherwise
-;;      (error "MUST-INIT unknown type: ~a (~a)" (type-of test) test))))
+(declaim (optimize (debug 3) (speed 0) (safety 3) (space 0) (compilation-speed 0)))
 
 (declaim (ftype (function (number) number) 2+))
 (defun 2+ (value)
@@ -30,12 +14,6 @@
   (let* ((real-path (concatenate 'string "assets/" path))
          (yag-path (asdf:system-relative-pathname "cl-yag" real-path)))
     (namestring yag-path)))
-
-(defmethod must-init ((test t) desc)
-  (unless test
-    (if (>  (al:get-errno) 0)
-        (error "Couldn't initialize ~s (~d)." desc (al:get-errno))
-        (error "Couldn't initialize ~s." desc))))
 
 (defun print-object-as-string (object)
   (with-output-to-string (s)
